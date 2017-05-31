1c339dda1a2f97995986a92b0af3f78aeedb0aac
André Luiz <andre.santos@tree.sh>
2017-05-31 20:11:25 -0300

    Evolução

--- channels_demo/consumers.py
+++ channels_demo/consumers.py
@@ -1,6 +1,18 @@
-def ws_message(message):
-    # ASGI WebSocket packet-received and send-packet message types
-    # both have a "text" key for their textual data.
-    message.reply_channel.send({
-        "text": message.content['text'][::-1],
+from channels import Group
+
+
+def ws_receive(message):
+    Group("chat").send({
+        "text": message.content['text'],
     })
+
+
+def ws_connect(message):
+    # Accept the incoming connection
+    message.reply_channel.send({'accept': True})
+    # Add them to the chat group
+    Group('chat').add(message.reply_channel)
+
+
+def ws_disconnect(message):
+    Group('chat').discard(message.reply_channel)
--- channels_demo/routing.py
+++ channels_demo/routing.py
@@ -1,8 +1,10 @@
 from channels.routing import route
 
-from .consumers import ws_message
+from .consumers import ws_connect, ws_disconnect, ws_receive
 
 
 channel_routing = [
-    route('websocket.receive', ws_message),
+    route('websocket.connect', ws_connect),
+    route('websocket.disconnect', ws_disconnect),
+    route('websocket.receive', ws_receive),
 ]
