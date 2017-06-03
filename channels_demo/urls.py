c827fcb108b94a03b2c79b294165eb67ac1c9980
André Luiz <contato@xdvl.info>
2017-06-03 16:39:10 -0300

    Remove rota antiga

--- channels_demo/urls.py
+++ channels_demo/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 
     url(r'^leilao/', include('channels_demo.leilao.urls', namespace='leilao')),
-    url(r'^chat/', include('channels_demo.chat.urls', namespace='chat')),
 
     url(r'^admin/', admin.site.urls),
 ]
